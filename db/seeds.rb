0818e0621cca9cf8d6042afa2af1e607ba6d9d3b
Min Kim <mk4555@nyu.edu>
2018-02-20 19:12:15 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+song = Song.create(name: "hi")
+artist = Artist.create(name: "Drake")
+song.artist = artist
