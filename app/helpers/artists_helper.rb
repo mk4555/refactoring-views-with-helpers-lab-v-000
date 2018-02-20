e001ee37ed83629e1db9a3980a301f00c90490d2
Min Kim <mk4555@nyu.edu>
2018-02-20 19:07:42 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,4 +1,8 @@
 module ArtistsHelper
   def display_artist
+    if self.artist
+      link_to self.aritst.name, artist_path self.artist
+    else
+      link_to "Add Artist", edit_song_path self
   end
 end
