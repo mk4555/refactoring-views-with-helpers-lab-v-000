f9b5798ee5f550fc1145809dc992778de411227b
Min Kim <mk4555@nyu.edu>
2018-02-20 19:09:47 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist
       link_to song.aritst.name, artist_path song.artist
     else
-      link_to "Add Artist", edit_song_path song
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
